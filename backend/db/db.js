3b251a4ddcd00a471598afd13d5f6e4487129d52
us-trigvent <umesh@trigvent.com>
2026-03-09 17:21:52 +0530

    changes for db

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const connectDB = ()=>{
-    mongoose.connect("mongodb+srv://umesh:Umesh123@umesh-cluster.sdppjkc.mongodb.net/uber").then(()=>{
+    mongoose.connect("mongodb://umesh:Umesh123@ac-qjirh4m-shard-00-00.sdppjkc.mongodb.net:27017,ac-qjirh4m-shard-00-01.sdppjkc.mongodb.net:27017,ac-qjirh4m-shard-00-02.sdppjkc.mongodb.net:27017/?ssl=true&replicaSet=atlas-134ha8-shard-0&authSource=admin&appName=umesh-cluster").then(()=>{
 console.log("Connected DB")
     }).catch((error)=>{
         console.log(error)
